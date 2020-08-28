95fc03292d3b0bd42a54c9c8dd739bedb51da60d
Diarte Jeffcoat <diartesjeffcoat@icloud.com>
2020-08-28 13:02:38 -0400

    Fixed issue with migration of Ticket model.

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from myusers.models import CustomUser
 
 # Create your models here.
+
+
+class BugTicket(models.Model):
+    NEW = 'N'
+    IN_PROGRESS = 'P'
+    DONE = 'D'
+    INVALID = 'I'
+
+    TICKET_STATUS_CHOICES = [
+        (NEW, 'New'),
+        (IN_PROGRESS, 'In Progress'),
+        (DONE, 'Done'),
+        (INVALID, 'Invalid'),
+    ]
+
+    title = models.CharField(max_length=150)
+    time_filed = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    submitted_by = models.ForeignKey(
+        CustomUser, related_name="submitted_by", on_delete=models.CASCADE, null=True)
+    status = models.CharField(
+        max_length=1, choices=TICKET_STATUS_CHOICES, default=NEW)
+    assigned_to = models.ForeignKey(
+        CustomUser, related_name="assigned_to", on_delete=models.CASCADE, null=True)
+    completed_by = models.ForeignKey(
+        CustomUser, related_name="completed_by", on_delete=models.CASCADE, null=True)
--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import login, logout, authenticate
+from myusers.models import CustomUser
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def user_detail_view(request, user_id):
+    pass
+
+
+def ticket_detail_view(request, ticket_id):
+    pass
+
+
+def register_view(request):
+    pass
+
+
+def login_view(request):
+    pass
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
